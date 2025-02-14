a342f9aa0ef99a458a2faca65bdb1e8a18937047
pepmiguelburguera <pepmiguelburguera@alumnat.iesdamiahuguet.net>
2025-02-14 13:05:52 +0100

    el treball

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log('')
+
+const paraulaperadivinar= 'pepot';
+
+const paraulaescrita='palma';
+
+
+function convertirMajuscules(paraula) {
+   return paraula.toLocaleUpperCase('paraula')
+}
+function midaParaula(paraula) {
+
+   //console.log(`La paraula ${paraula} te ${paraula.length} caracters`);
+   return paraula.length;
+}
+
+
+let z = convertirMajuscules('pepot')
+let y = convertirMajuscules('palma')
+console.log(z);
+console.log(y);
+
+let x= midaParaula('pepot')
+console.log(x)
+let b =midaParaula('palma')
+console.log(b)
+
+
+//for (let i=0;)
+/*if (condition1) {
+    //  block of code to be executed if condition1 is true
+} else if (condition2) {
+    //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+    //  block of code to be executed if the condition1 is false and condition2 is false
+}
+*/
+
+
+
+
+
+
+
+
