bb6677e4d128663d1b4dc0ef9adb64d88c74b2c0
pepmiguelburguera <pepmiguelburguera@alumnat.iesdamiahuguet.net>
2025-02-21 13:05:42 +0100

    bo

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 let paraulaperadivinar= 'pepot';
+function Revisar() {
+  paraulaescrita = document.getElementById("caixa").innerHTML;
 
-let paraulaescrita='palma';
+   paraulaperadivinar = paraulaperadivinar.toUpperCase();
+   paraulaescrita = paraulaescrita.toUpperCase();
+   console.log(paraulaperadivinar);
+   console.log(paraulaescrita);
 
-paraulaperadivinar = paraulaperadivinar.toUpperCase();
-paraulaescrita = paraulaescrita.toUpperCase();
-console.log(paraulaperadivinar);
-console.log(paraulaescrita);
+   if (paraulaescrita.length == paraulaperadivinar.length) {
+      //continua
+      for (let posicióparaula = 0; posicióparaula < paraulaescrita.length; posicióparaula += 1) {
+         if (paraulaperadivinar.charAt(posicióparaula) == paraulaescrita.charAt(posicióparaula)) {
+            //verd
+            console.log('verd');
+         } else if (paraulaperadivinar.includes(paraulaescrita.charAt(posicióparaula))) {
+            //groc
+            console.log('groc');
+         } else {
+            //vermell
+            console.log('gris');
+         }
 
-if(paraulaescrita.length == paraulaperadivinar.length){
-   //continua
-   for(let posicióparaula=0; posicióparaula < paraulaescrita.length; posicióparaula += 1){
-      if(paraulaperadivinar.charAt(posicióparaula) == paraulaescrita.charAt(posicióparaula)){
-         //verd
-         console.log('verd');
       }
-      else if(paraulaperadivinar.includes(paraulaescrita.charAt(posicióparaula))){
-         //groc
-         console.log('groc');
-      }
-      else{
-         //vermell
-         console.log('gris');
-      }
 
+   } else {
+      //error
+      //tornar a introduir paraula escrita
+      //alert('Error');
+      console.log('Error');
    }
-
 }
-else{
-   //error
-   //tornar a introduir paraula escrita
-   //alert('Error');
-   console.log('Error');
-}
-
 //Si son iguales en verd
 //si hi es però no a la mateixa
